209fb24c139d0060aa41073bf71beae05cb59cd1
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2025-12-24 16:37:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfYear(Number) {
+	if(Number%400 ===0){
+		return 366;
+	}else if(Number%4 ===0 && Number%100 !==0){
+		return 366;
+	}else{
+		return 365;
+	}
+}
